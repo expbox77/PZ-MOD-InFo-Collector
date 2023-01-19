cdfd19b0bf3857e1022893420bcf7cbc78c59a78
ExpBox77 <51470233+expbox77@users.noreply.github.com>
2023-01-19 20:26:17 +0900

    Update db.py

--- script/db.py
+++ script/db.py
@@ -1,20 +1,21 @@
 import sqlite3
 
 def connect_db(db_name):
-    # DB 생성 (오토 커밋)
-    conn_db = sqlite3.connect(f'{db_name}.db', isolation_level=None)
-
-    # 커서 획득
-    return conn_db.cursor()
+    try:
+        # DB 생성 (오토 커밋)
+        conn_db = sqlite3.connect(f'{db_name}.db', isolation_level=None)
+        return conn_db
+    except Exception as e:
+        print(e)
 
 def make_db(db_name):
-    c = connect_db(db_name)
+    conn = connect_db(db_name).cursor()
 
     # 테이블 생성 (데이터 타입은 TEST, NUMERIC, INTEGER, REAL, BLOB 등)
-    c.execute("CREATE TABLE IF NOT EXISTS ZombieMOD \
+    conn.execute("CREATE TABLE IF NOT EXISTS ZombieMOD \
         (id integer PRIMARY KEY, title text, description text, time_updated integer)")
     
-    c.close()
+    conn.close()
 
 
 def insert_db(db_name, mod_id, json_title, json_description, json_time_updated):
